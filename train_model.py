9765b8778ce72653b4ed77845e4098a7f4e4240b
Nandhita-Bodapatruni <325203320006@gvpce.ac.in>
2026-06-14 17:05:10 +0530

    Fraud Distribution Graph

--- train_model.py
+++ train_model.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 # Load dataset
 df = pd.read_csv("data/creditcard.csv")
@@ -11,3 +13,19 @@ print(df.columns)
 print("\nFraud Distribution:")
 print(df["Class"].value_counts())
 
+print("\nMissing Values:")
+print(df.isnull().sum().sum())
+
+print("\nDataset Info:")
+print(df.info())
+
+fraud_counts = df["Class"].value_counts()
+
+plt.figure(figsize=(6,4))
+fraud_counts.plot(kind="bar")
+
+plt.title("Fraud vs Genuine Transactions")
+plt.xlabel("Class")
+plt.ylabel("Count")
+
+plt.show()
